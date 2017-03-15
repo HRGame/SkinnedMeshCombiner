195346861ec9d4a873c6b02cced3a9c33a335e36
JO3L.islet8 <islet8@ymail.com>
2017-03-15 13:11:44 +0800

    Tthe first version.

---
+++ Assets/PlanarShadows/Example/Example.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+	public Light ShadowingLight;
+	public Color ShadowColor;
+	public GameObject ShadowingObjectsRootGO;
+
+	void OnEnable()
+	{
+		_EnablePlanarShadow( true );
+	}
+
+	void OnDisable()
+	{
+		_EnablePlanarShadow( false );
+	}
+
+	void _EnablePlanarShadow( bool enabled )
+	{
+		for( int i = 0; i < ShadowingObjectsRootGO.transform.childCount; ++i )
+		{
+			GameObject shadowingObjectGO = ShadowingObjectsRootGO.transform.GetChild( i ).gameObject;
+			PlanarShadows.ShadowingLightDirection = ShadowingLight.transform.forward;
+			PlanarShadows.ShadowColor = ShadowColor;
+			PlanarShadows.EnableShadowOnGameObject( shadowingObjectGO, enabled );
+		}
+	}
+}
